a82e4ff453dc0623ba176f52f9359f9cbb7b63ef
redawaseem <65788008+redawaseem@users.noreply.github.com>
2020-10-16 06:58:02 -0400

    good ole styling

---
+++ script/app.js
@@ -0,0 +1,14 @@
+
+
+
+const navSlide = () => {
+    const burger = document.querySelector('.burgerMenu');
+    const nav = document.querySelector('.navBar');
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('navBar-active');
+        // burger animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
